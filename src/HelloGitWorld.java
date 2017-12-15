07cb25c12f0a5fc42786f762ab6190f78fc1ee29
omj9803 <omj1004v@Naverm.com>
2017-12-15 22:15:58 +0900

    update student number

--- src/HelloGitWorld.java
+++ src/HelloGitWorld.java
@@ -1,7 +1,6 @@
 
 public class HelloGitWorld {
 	public static void main(String args[]) {
-        System.out.println("Hello, Git World!");
-        System.out.println("201702083");
-    }
+	System.out.println("201702039");
+	}
 }
